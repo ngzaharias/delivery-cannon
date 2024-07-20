d08ae75c29845bff6c9aa05619e0208c8e1b4ece
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2024-07-19 22:37:40 -0400

    projectile can now be fired

---
+++ prototypes/delivery-cannon-capsule.lua
@@ -0,0 +1,108 @@
+data:extend({
+	{
+		type = "recipe",
+		name = "delivery-cannon-capsule",
+		result = "delivery-cannon-capsule",
+		enabled = false,
+		energy_required = 10,
+		ingredients = {
+			{ "low-density-structure", 1 },
+			{ "explosives", 5 },
+			{ "copper-cable", 10 },
+		},
+		requester_paste_multiplier = 1,
+		always_show_made_in = false,
+	},
+	{
+		type = "item",
+		name = "delivery-cannon-capsule",
+		icon = "__delivery-cannon__/graphics/icons/delivery-cannon-capsule.png",
+		icon_size = 64,
+		order = "s",
+		subgroup = "logistic-network",
+		stack_size = 50,
+	},
+	{
+		-- making these artillery-projectile(s) instead of projectile(s) makes it possible for them to show up on the map
+		type = "artillery-projectile",
+		name = "delivery-cannon-projectile",
+		icon = "__delivery-cannon__/graphics/icons/delivery-cannon-capsule.png",
+		icon_size = 64,
+		acceleration = 0,
+		rotatable = false,
+		picture = {
+			filename = "__delivery-cannon__/graphics/entity/delivery-cannon/delivery-cannon-capsule.png",
+			width = 58/2,
+			height = 94/2,
+			priority = "high",
+			shift = { 0, 0 },
+			{
+				filename = "__delivery-cannon__/graphics/entity/delivery-cannon/hr-delivery-cannon-capsule.png",
+				width = 58,
+				height = 94,
+				priority = "high",
+				shift = { 0, 0 },
+				scale = 0.5,
+			},
+		},
+		-- reveal_map, map_color, and chart_picture are specific to artillery-projectile and dictate how it will behave with the map
+		-- because charting is handled by the delivery cannon script, reveal_map should be left false
+		reveal_map = false,
+		map_color = {r=0.3, g=0.6, b=0.1},
+		chart_picture =	{
+			filename = "__base__/graphics/entity/artillery-projectile/artillery-shoot-map-visualization.png",
+			flags = { "icon" },
+			frame_count = 1,
+			width = 64,
+			height = 64,
+			priority = "high",
+			scale = 0.20,
+			tint = {r=0.3, g=0.6, b=0.1},
+		},
+		flags = { "not-on-map", "placeable-off-grid"},
+		light = { intensity = 0.2, size = 10},
+		smoke = {
+			{
+				deviation = { 0.15, 0.15 },
+				frequency = 1,
+				name = "smoke-fast",
+				--name = "smoke-explosion-particle",
+				--name = "soft-fire-smoke", -- lasts longer
+				position = { 0, 0},
+				slow_down_factor = 1,
+				starting_frame = 3,
+				starting_frame_deviation = 5,
+				starting_frame_speed = 0,
+				starting_frame_speed_deviation = 5
+			}
+		},
+	},
+	{
+		type = "projectile",
+		name = "delivery-cannon-projectile-shadow",
+		acceleration = 0,
+		rotatable = false,
+		animation = {
+			draw_as_shadow = true,
+			filename = "__delivery-cannon__/graphics/entity/delivery-cannon/delivery-cannon-capsule-shadow.png",
+			frame_count = 1,
+			width = 98/2,
+			height = 50/2,
+			line_length = 1,
+			priority = "high",
+			shift = { 0, 0 },
+			hr_version = {
+				draw_as_shadow = true,
+				filename = "__delivery-cannon__/graphics/entity/delivery-cannon/hr-delivery-cannon-capsule-shadow.png",
+				frame_count = 1,
+				width = 98,
+				height = 50,
+				line_length = 1,
+				priority = "high",
+				shift = { 0, 0 },
+				scale = 0.5,
+			},
+		},
+		flags = { "not-on-map", "placeable-off-grid"},
+	  },
+})
--- scripts/DeliveryCannon.lua
+++ scripts/DeliveryCannon.lua
@@ -1,7 +1,12 @@
 local DeliveryCannon = {}
-DeliveryCannon.name = "delivery-cannon"
-DeliveryCannon.beam_name = "delivery-cannon-beam"
-DeliveryCannon.beam_offset = { x = -0.5, y = -6 },
+DeliveryCannon.m_Name = "delivery-cannon"
+DeliveryCannon.m_BeamName = "delivery-cannon-beam"
+DeliveryCannon.m_BeamOffset = { x = -0.5, y = -6 }
+DeliveryCannon.m_CapsuleName = "delivery-cannon-capsule"
+DeliveryCannon.m_ProjectileName = "delivery-cannon-projectile"
+DeliveryCannon.m_ProjectileShadowName = "delivery-cannon-projectile-shadow"
+DeliveryCannon.m_CapsuleTimer = 2 * 60
+DeliveryCannon.m_CapsuleAltitude = 100
 
 script.on_init(function()
 	DeliveryCannon.OnInit()
@@ -19,43 +24,35 @@ function DeliveryCannon.OnEntityCreated(event)
 	local entity = Util.GetEntityFromEvent(event)
 	if not entity then 
 		return end
-	if entity.name ~= DeliveryCannon.name then 
+	if entity.name ~= DeliveryCannon.m_Name then 
 		return end
 	game.print("OnEntityCreated: "..entity.name)
 
     ---@type DeliveryCannonInfo
 	local deliveryCannon = {
 		m_Entity = entity,
-		m_UnitNumber = entity.unit_number,
+		m_Handle = entity.unit_number,
+		m_Target = { position = { x = 0, y = 0 }, surface = entity.surface }
 	}
 
 	global.s_DeliveryCannons[entity.unit_number] = deliveryCannon
 end
-script.on_event(defines.events.on_entity_cloned, DeliveryCannon.OnEntityCreated)
-script.on_event(defines.events.on_built_entity, DeliveryCannon.OnEntityCreated)
-script.on_event(defines.events.on_robot_built_entity, DeliveryCannon.OnEntityCreated)
-script.on_event(defines.events.script_raised_built, DeliveryCannon.OnEntityCreated)
-script.on_event(defines.events.script_raised_revive, DeliveryCannon.OnEntityCreated)
 
 ---@param event EntityRemovalEvent
 function DeliveryCannon.OnEntityRemoved(event)
 	local entity = event.entity
 	if not entity then 
 		return end
-	if entity.name ~= DeliveryCannon.name then 
+	if entity.name ~= DeliveryCannon.m_Name then 
 		return end
 	game.print("OnEntityRemoved: " .. entity.name)
 
 	global.s_DeliveryCannons[entity.unit_number] = nil
 end
-script.on_event(defines.events.on_entity_died, DeliveryCannon.OnEntityRemoved)
-script.on_event(defines.events.on_robot_mined_entity, DeliveryCannon.OnEntityRemoved)
-script.on_event(defines.events.on_player_mined_entity, DeliveryCannon.OnEntityRemoved)
-script.on_event(defines.events.script_raised_destroy, DeliveryCannon.OnEntityRemoved)
 
 function DeliveryCannon.OnTick(event)
 	for _, deliveryCannon in pairs(global.s_DeliveryCannons) do
-		if (event.tick + deliveryCannon.m_UnitNumber) % 60 == 0 then
+		if (event.tick + deliveryCannon.m_Handle) % 60 == 0 then
 			DeliveryCannon.AttemptToFire(deliveryCannon)
 		end
 	end
@@ -67,12 +64,76 @@ script.on_event(defines.events.on_tick, DeliveryCannon.OnTick)
 function DeliveryCannon.AttemptToFire(deliveryCannon)
 	--game.print("AttemptToFire")
 
+	if not deliveryCannon.m_Target then
+		return end
+
+	local stack = DeliveryCannon.GetStack(deliveryCannon)
+	if not stack then 
+		return end
+
+		stack.count = stack.count - 1
+
+	---@type DeliveryCannonPayloadInfo
+	local payload = {
+		stack = stack,
+		timer = game.tick + DeliveryCannon.m_CapsuleTimer,
+		health = 1
+	}
+	table.insert(global.s_DeliveryCannonPayloads, payload)
+
 	deliveryCannon.m_Entity.create_build_effect_smoke()
 	deliveryCannon.m_Entity.surface.create_entity {
-		name = DeliveryCannon.beam_name,
-		position = Util.Vector2Add(deliveryCannon.m_Entity.position, DeliveryCannon.beam_offset ),
-		target = Util.Vector2Add(deliveryCannon.m_Entity.position, {x = 0, y = -100})
+		name = DeliveryCannon.m_BeamName,
+		position = Util.Vector2Add(deliveryCannon.m_Entity.position, DeliveryCannon.m_BeamOffset ),
+		target = Util.Vector2Add(deliveryCannon.m_Entity.position, {x = 0, y = -200})
 	}
+
+	local speed = DeliveryCannon.m_CapsuleAltitude / DeliveryCannon.m_CapsuleTimer
+	local targetPosition = deliveryCannon.m_Target.position
+	local targetSurface = deliveryCannon.m_Target.surface
+	local capsuleStartPosition = Util.Vector2Add(targetPosition, { x = 0, y = -DeliveryCannon.m_CapsuleAltitude })
+	local shadowStartPosition = Util.Vector2Add(targetPosition, { x = DeliveryCannon.m_CapsuleAltitude, y = 0 })
+	targetSurface.create_entity {
+		name = DeliveryCannon.m_ProjectileName,
+		position = capsuleStartPosition,
+		target = targetPosition,
+		speed = speed
+	}
+	targetSurface.create_entity {
+		name = DeliveryCannon.m_ProjectileShadowName,
+		position = shadowStartPosition,
+		target = targetPosition,
+		speed = speed
+	}
+	targetSurface.request_to_generate_chunks(capsuleStartPosition)
+	targetSurface.request_to_generate_chunks(shadowStartPosition)
+	targetSurface.request_to_generate_chunks(targetPosition)
 end
+
+---@param deliveryCannon DeliveryCannonInfo
+---@return ItemStackDefinition?
+function DeliveryCannon.GetStack(deliveryCannon)
+	if not deliveryCannon then 
+		return end
+
+	local inventory = deliveryCannon.m_Entity.get_output_inventory()
+	if not inventory then 
+		return end
+	if inventory.is_empty() then 
+		return end
+
+	return inventory[1]
+end
+
+script.on_event(defines.events.on_entity_cloned, DeliveryCannon.OnEntityCreated)
+script.on_event(defines.events.on_built_entity, DeliveryCannon.OnEntityCreated)
+script.on_event(defines.events.on_robot_built_entity, DeliveryCannon.OnEntityCreated)
+script.on_event(defines.events.script_raised_built, DeliveryCannon.OnEntityCreated)
+script.on_event(defines.events.script_raised_revive, DeliveryCannon.OnEntityCreated)
+
+script.on_event(defines.events.on_entity_died, DeliveryCannon.OnEntityRemoved)
+script.on_event(defines.events.on_robot_mined_entity, DeliveryCannon.OnEntityRemoved)
+script.on_event(defines.events.on_player_mined_entity, DeliveryCannon.OnEntityRemoved)
+script.on_event(defines.events.script_raised_destroy, DeliveryCannon.OnEntityRemoved)
 
 return DeliveryCannon
